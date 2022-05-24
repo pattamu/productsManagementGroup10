76908a65249075b5aa7e396755c4531d11b6b1c1
pattamu <sandeepkumar416166@gmail.com>
2022-05-24 15:02:54 +0530

    code updated

--- src/routers/routes.js
+++ src/routers/routes.js
@@ -2,9 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const {createUser} = require('../controllers/userController')
+const {userLogin} = require('../controllers/loginController')
+const {userAuthentication} = require('../middlewares/authentication')
+
 
 //User API Route Handlers
 router.post('/register', createUser)
-
+router.post('/login', userLogin)
 
 module.exports = router
