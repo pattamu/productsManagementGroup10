9f3b15fc8d0adf5b152b24757f728cef7e731756
pattamu <sandeepkumar416166@gmail.com>
2022-05-25 22:56:42 +0530

    productController added

---
+++ src/controllers/productController.js
@@ -0,0 +1,196 @@
+const mongoose = require('mongoose')
+const uploadFile = require('./awsConnect')
+const productModel = require('../models/productModel')
+const {isFileImage, isValid} = require('../validation/validator')
+
+const createProduct = async (req, res) => {
+    try{
+        let data = JSON.parse(JSON.stringify(req.body))
+        let error = []
+
+        //checking if body is empty
+        if(!Object.keys(data).length)
+            return res.status(400).send({status: false, message: "Enter data to create User."})
+
+        //check for title
+        if(!isValid(data.title)) error.push('Title is required')
+        //check for description
+        if(!isValid(data.description)) error.push('Description is required')
+        //check for title
+        if(!isValid(data.price)) error.push('Price is required')
+        //check price validity
+        if(isValid(data.price) && (isNaN(data.price) || parseInt(data.price) < 0)) error.push('Price should be a +ve Integer')
+
+        //check if currencyId is 'INR' Only
+        if(isValid(data.currencyId) && data.currencyId != 'INR') error.push("CurrencyId can only be 'INR'")
+        //check if currencyFormat is '₹' Only
+        if(isValid(data.currencyFormat) && data.currencyFormat != '₹') error.push("currencyFormat can only be '₹'")
+
+        //check if isFreeShipping is Boolean
+        if(isValid(data.isFreeShipping) && !['true','false'].includes(data.isFreeShipping))
+            error.push('isFreeShipping must be a Boolean value')
+
+        //check if file is present
+        if(!req.files.length)
+            error.push("Image file is required")
+        //check if file is an image (Remeber 'field name' in postman is optional while uploading file)
+        if(req.files.length){
+            let check = isFileImage(req.files[0])
+            if(!check) 
+                error.push('Invalid file, Image only allowed')
+        }
+
+        //check for AvailableSizes
+        if(!isValid(data.availableSizes)) error.push('AvailableSizes is required')
+
+        if(isValid(data.availableSizes)){
+            let arr = ["S", "XS","M","X", "L","XXL", "XL"]
+            data.availableSizes = data.availableSizes.split(/[",\[\]]/).filter(x=>x.trim())
+            if(data.availableSizes.some(x => !arr.includes(x.trim())))
+                error.push('Size can only be from: S, XS, M, X, L, XXL, XL')
+        }
+
+        if(error.length == 1)
+            return res.status(400).send({status: false, message: error.toString()})
+        else if(error.length > 1)
+            return res.status(400).send({status: false, message: error})
+
+        data.availableSizes = [...new Set(data.availableSizes)]
+        data.productImage = await uploadFile(req.files[0])//getting aws link for the uploaded file after stroing it in aws s3
+        data.isDeleted = false
+        const created = await productModel.create(data)
+        res.status(201).send({status: true, message: "Product created succefully", data: created})
+    }catch(err){
+        res.status(500).send({status: false, message: err.message})
+    }
+}
+
+
+const getProducts = async(req, res) => {
+    try{
+        let filters = req.query
+        let options, newFilter = {title: filters.name}
+
+        if(isValid(filters.size))
+            options = filters.size.split(/[, '"+-;]+/).map(x => {return x.trim() && {availableSizes:x}})
+        
+        if(isValid(filters.priceGreaterThan)) newFilter.price = {$gt: filters.priceGreaterThan}
+        if(isValid(filters.priceLessThan)) newFilter.price = {$lt: filters.priceLessThan}
+        if(isValid(filters.priceLessThan) && isValid(filters.priceGreaterThan)) 
+            newFilter.price = { $gt: filters.priceGreaterThan, $lt: filters.priceLessThan }
+
+        Object.keys(newFilter).forEach(key => !isValid(newFilter[key]) && delete newFilter[key])
+
+        if (isValid(filters.priceSort) && !(filters.priceSort == -1 || filters.priceSort == 1))
+            return res.status(400).send({ status: false, message: "You Can Only Use 1 For Ascending And -1 For Descending Sorting" })
+        
+        if(!Object.keys(newFilter).length && !options?.length){
+            let products = await productModel.find({isDeleted: false},{__v: 0}).sort({price: filters.priceSort})
+            if(!products.length)
+                return res.status(404).send({ status: false, message: "Product not found." })
+            res.status(200).send({status: true, message: "Product data fetched.", data: products})
+        }
+        else{
+            let products = await productModel.find({$and: [newFilter, {$or:options}, {isDeleted: false}]},{__v: 0}).sort({price: filters.priceSort})
+            if(!products.length)
+                return res.status(404).send({ status: false, message: "Product not found." })
+            res.status(200).send({status: true, message: "Product data fetched.", data: products})
+        }
+
+    }catch(err){
+        res.status(500).send({status: false, message: err.message})
+    }
+}
+
+
+const getProduct = async (req, res) => {
+    try{
+        let pId = req.params.productId
+
+        if(!mongoose.isValidObjectId(pId))
+            return res.status(401).send({status: false, message: `'${pId}' is an Invalid ProductId.`})
+
+        let product = await productModel.findById(pId)
+
+        if(!product || product.isDeleted)
+            return res.status(404).send({ status: false, message: "Product not found." })
+
+        res.status(200).send({status: true, message: "Product data fetched.", data: product})
+
+    }catch(err){
+        res.status(500).send({status: false, message: err.message})
+    }
+}
+
+
+const updateProduct = async (req, res) => {
+    try{
+        let pId = req.params.productId
+        let data = JSON.parse(JSON.stringify(req.body))
+        delete data.isDeleted
+        delete data.deletedAt
+        let error = []
+
+        if(!mongoose.isValidObjectId(pId))
+            return res.status(401).send({status: false, message: `'${pId}' is an Invalid ProductId.`})
+
+        let findProduct = await productModel.findOne({_id: pId, isDeleted: false})
+        if(!findProduct)
+            return res.status(404).send({status: false, message: `No product found for: '${pId}'`})
+        
+        if(!Object.keys(data).length)
+            return res.status(400).send({status: false, message: "Can't update product without data."})
+
+        if(isValid(data.availableSizes)){
+            let arr = ["S", "XS","M","X", "L","XXL", "XL"]
+            data.availableSizes = data.availableSizes.split(/[",\[\]]/).filter(x=>x.trim())
+            if(data.availableSizes.some(x => !arr.includes(x.trim())))
+                error.push('Size can only be from: S, XS, M, X, L, XXL, XL')
+        }
+
+        //check if file is an image (Remeber 'field name' in postman is optional while uploading file)
+        if(req.files.length){
+            if(!isFileImage(req.files[0])) 
+                error.push('Invalid file, Image only allowed')
+            else 
+                data.productImage = await uploadFile(req.files[0])
+        }
+
+        if(isValid(data.price) && isNaN(data.price)) error.push('Price should be an Integer')
+
+        if(error.length == 1)
+            return res.status(400).send({status: false, message: error.toString()})
+        else if(error.length > 1)
+            return res.status(400).send({status: false, message: error})
+
+        data['$addToSet'] = {availableSizes: {$each:data.availableSizes||[]}}
+        delete data.availableSizes
+        let updatedProduct = await productModel.findOneAndUpdate({_id:pId},data,{new:true})
+        res.status(200).send({status: true, message: 'successfully updated', data: updatedProduct})
+
+    }catch(err){
+        res.status(500).send({status: false, message: err.message})
+    }
+}
+
+
+const deleteProduct = async (req, res) => {
+    try{
+        let pId = req.params.productId
+
+        if(!mongoose.isValidObjectId(pId))
+            return res.status(401).send({status: false, message: `'${pId}' is an Invalid ProductId.`})
+
+        let deletedProduct = await productModel.findOneAndUpdate({_id: pId, isDeleted: false},{isDeleted: true, deletedAt: Date.now()},{new: true})
+
+        if(!deletedProduct)
+            return res.status(404).send({ status: false, message: "Product not found." })
+
+        res.status(200).send({status: true, message: "Product deleted successfully.", data: deletedProduct})
+
+    }catch(err){
+        res.status(500).send({status: false, message: err.message})
+    }
+}
+
+module.exports = {createProduct, getProducts, getProduct, updateProduct, deleteProduct}
--- src/routers/routes.js
+++ src/routers/routes.js
@@ -2,14 +2,25 @@ const express = require('express');
 const router = express.Router();
 
 const {createUser, getUser, updateUser} = require('../controllers/userController')
+const {createProduct, getProducts, getProduct, updateProduct, deleteProduct} = require('../controllers/productController')
+// const {createCart} = require('../controllers/cartController')
 const {userLogin} = require('../controllers/loginController')
 const {userAuthentication} = require('../middlewares/authentication')
 
-
-//User API Route Handlers
+//User API Routes
 router.post('/register', createUser)
 router.post('/login', userLogin)
 router.get('/user/:userId/profile', userAuthentication, getUser)
 router.put('/user/:userId/profile', userAuthentication, updateUser)
+
+//Product API Routes
+router.post('/products', createProduct)
+router.get('/products', getProducts)
+router.get('/products/:productId', getProduct)
+router.put('/products/:productId', updateProduct)
+router.delete('/products/:productId', deleteProduct)
+
+//Cart Api Routers
+// router.post('/users/:userId/cart', userAuthentication, createCart)
 
 module.exports = router
